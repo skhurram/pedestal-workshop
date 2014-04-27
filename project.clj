7bb7c010a809fe38d403fb44ce5839dd7a136116
mdk <khurram@syedkhurram.com>
2014-04-27 19:11:44 +0200

    environ lib

--- project.clj
+++ project.clj
@@ -3,8 +3,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
+  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                   :username "myemail"
+                                   :password "mypassword"}}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.datomic/datomic-free "0.9.4699"
+                 [com.datomic/datomic-pro "0.9.4384"
                   :exclusions [org.slf4j/jul-to-slf4j
                                org.slf4j/slf4j-nop]]
                  [io.pedestal/pedestal.service "0.3.0-SNAPSHOT"]
@@ -12,5 +15,10 @@
                  [io.pedestal/pedestal.jetty "0.3.0-SNAPSHOT"]
                  [ns-tracker "0.2.2"]
                  [ring/ring-devel "1.2.2"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [environ "0.5.0"]]
+
+  :plugins [[lein-environ "0.5.0"]]
+
   :main ^{:skip-aot true} todoit.core)
+
